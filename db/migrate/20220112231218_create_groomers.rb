eabde75ed7b411ffa9612d19f32336d325e03cc1
Mike <mikekett4@yahoo.com>
2022-01-12 17:00:24 -0700

    completed DB creation, schema created

--- db/migrate/20220112231218_create_groomers.rb
+++ db/migrate/20220112231218_create_groomers.rb
@@ -2,7 +2,7 @@ class CreateGroomers < ActiveRecord::Migration[6.1]
   def change
     create_table :groomers do |t|
       t.string :name
-      t.int :experience
+      t.integer :experience
 
       t.timestamps
     end
