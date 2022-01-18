ad3ff1b7ab937bf73339344e3dd55228696f3495
Mike <mikekett4@yahoo.com>
2022-01-17 17:14:14 -0700

    seeded groomers and services

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Groomer.create()
+puts "🌱 Seeding spices..."
+
+puts "seeding groomers"
+Groomer.create(name: "Joyce", experience: 10)
+Groomer.create(name: "Randy", experience: 3)
+Groomer.create(name: "Paula", experience: 2)
+Groomer.create(name: "Matt", experience: 5)
+Groomer.create(name: "Carl", experience: 7)
+Groomer.create(name: "Dianna", experience: 12)
+
+
+
+puts "seeding services"
+Service.create(name: "Nail Trimming", price: 10.00)
+Service.create(name: "Bath", price: 20.00)
+Service.create(name: "Bath & Nails", price: 25.00)
+Service.create(name: "Brush Out", price: 10.00)
+Service.create(name: "Cologne", price: 5.00)
+Service.create(name: "Ear Cleaning", price: 5.00)
+Service.create(name: "Teeth Brushing", price: 5.00)
+Service.create(name: "Top Dog", price: 50.00)
+
+
+puts "✅ Done seeding!"
