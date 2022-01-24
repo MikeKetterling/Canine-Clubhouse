b70e20030ff20609afe483fcb9cdd665c89782e4
Mike <mikekett4@yahoo.com>
2022-01-24 11:25:37 -0700

    push to heroku

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,4 +1,11 @@
 class Dog < ApplicationRecord
   belongs_to :user
   has_many :service_appointments, dependent: :destroy
+
+  has_one_attached :pet_image
+  validate :acceptable_image
+
+  def acceptable_image
+    return unless pet_image.attached?
+  end
 end
